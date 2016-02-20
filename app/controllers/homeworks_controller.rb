1308dacbee62f519531802189a43fb753422358a
ChihMin <s102062111@m102.nthu.edu.tw>
2016-02-20 15:32:24 +0800

    create homework and show contents of homeworks

--- app/controllers/homeworks_controller.rb
+++ app/controllers/homeworks_controller.rb
@@ -1,5 +1,33 @@
 class HomeworksController < ApplicationController
   def index
 	flash[:notice] = "你還有作業還沒交！"
+    @homeworks = Homework.all	
   end
+
+  def new
+    @homework = Homework.new
+  end
+
+  def create
+    @homework = Homework.create(homework_params)
+    if @homework.save
+      redirect_to homework_path(@homework.id)
+    else
+      render :new
+    end
+  end
+  
+  def show
+    @homework = Homework.find(params[:id])
+  end
+
+  def edit
+    @homework = Homework.find(params[:id])
+  end
+
+private
+  
+  def homework_params
+    params.require(:homework).permit(:title, :description)
+  end  
 end
