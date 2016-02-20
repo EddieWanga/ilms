1df6ad6921cd154bb48143dab10168fa4624b048
ChihMin <s102062111@m102.nthu.edu.tw>
2016-02-20 20:43:27 +0800

    limit size of uploaded files

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,3 +1,9 @@
 class Answer < ActiveRecord::Base
-  mount_uploader :attachment, AttachmentUploader 
+  mount_uploader :attachment, AttachmentUploader
+  validate :attachment_size_validation, :if => "attachment?"
+  def attachment_size_validation
+    if attachment.size > 50.megabytes
+	  errors.add(:base, "Attachment should be less than 10MB")
+	end
+  end 
 end
