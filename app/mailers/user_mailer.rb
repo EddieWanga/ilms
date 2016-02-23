70b057dd2c90cc3971c8ff21aeba2ca398893952
ChihMin <s102062111@m102.nthu.edu.tw>
2016-02-23 23:21:36 +0800

    complete submit answer & email to students, but there're some bugs for attachment downloads

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,6 @@
 class UserMailer < ApplicationMailer
-  default from: "foy2803@gmail.com"
+  default from: "sprout@csie.ntu.edu.tw"
+  add_template_helper(ApplicationHelper)
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
@@ -8,5 +9,13 @@ class UserMailer < ApplicationMailer
   def notify_comment
     @greeting = "Hi"
     mail(to: "hkhs7821@gmail.com", subject: "Welcome to sprout!")
+  end
+
+  def notify_submit(user, answer)
+    @answer = answer
+    @user = user
+    @homework = answer.homework
+    @url = @answer.attachment.url
+    mail(to: user.email, subject: "[資訊之芽繳交作業留存] #{@homework.title}")
   end
 end
