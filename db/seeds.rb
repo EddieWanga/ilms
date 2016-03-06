af568efb9825e59324ecc9aaa2df177894c25a04
ChihMin <s102062111@m102.nthu.edu.tw>
2016-03-06 12:14:36 +0800

    fix new homework button layout

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,7 @@
 
 User.create(email: "teacher@gmail.com", name: "Teacher", role: 0, password: "123456789")
 names = ["hkhs7821", "foy2803", "s102062111", "sproutstudent"] 
+current_id = 0
 names.each do |name|
   email = nil
   if name != "s102062111"
@@ -17,13 +18,20 @@ names.each do |name|
   end
   password = "123456789"
   confirm_code = SecureRandom.base64(60)
+  
+  dist = "CLang"
+  if current_id % 2 == 0
+    dist = "PyLang"
+  end
   User.create(
     email: email, 
     name: name, 
     role: 1, 
     password: password, 
-    confirm_code: confirm_code
+    confirm_code: confirm_code,
+    district: dist
   )
   Dictionary.create(confirm_code: confirm_code, password: password)
+  current_id = current_id + 1
 end
 
