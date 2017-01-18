0f82322d4b65fb2def7dc3f797ea2aa27d61b76f
ChihMin <s102062111@m102.nthu.edu.tw>
2017-01-18 21:59:26 +0800

    remove other test account from db/seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,31 +7,4 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 User.create(email: "teacher@gmail.com", name: "Teacher", role: 0, password: "123456789")
-names = ["hkhs7821", "foy2803", "s102062111"] 
-current_id = 0
-names.each do |name|
-  email = nil
-  if name != "s102062111"
-    email = name + "@gmail.com"
-  else
-    email = name + "@gapp.nthu.edu.tw"
-  end
-  password = "123456789"
-  confirm_code = SecureRandom.base64(60)
-  
-  dist = "CLang"
-  if current_id % 2 == 0
-    dist = "PyLang"
-  end
-  User.create(
-    email: email, 
-    name: name, 
-    role: 1, 
-    password: password, 
-    confirm_code: confirm_code,
-    district: dist
-  )
-  Dictionary.create(confirm_code: confirm_code, password: password)
-  current_id = current_id + 1
-end
 
